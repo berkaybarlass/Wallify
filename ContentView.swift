fccbd892f467e5618daf1727eedd09a586333f06
Berkay Barlas <74761090+berkaybarlass@users.noreply.github.com>
2023-11-26 20:11:25 +0300

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wallify
+//
+//  Created by BERKAY BARLAS on 26.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WallifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallifyApp.swift
+//  Wallify
+//
+//  Created by BERKAY BARLAS on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WallifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
