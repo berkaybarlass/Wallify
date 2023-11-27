228c5e5ec05bf870bccea829e1604e1927e7d7b7
Berkay Barlas <74761090+berkaybarlass@users.noreply.github.com>
2023-11-28 00:33:40 +0300

    [Components] Added wallpaper detail card and header text

---
+++ Components/HeaderText.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderText.swift
+//  Wallify
+//
+//  Created by BERKAY BARLAS on 27.11.2023.
+//
+
+import SwiftUI
+
+struct HeaderText: View {
+    var body: some View {
+        Text("Abstract Wallpaper")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .fontWeight(.heavy)
+            .foregroundColor(Color("Primary"))
+    }
+}
---
+++ Components/WallpaperCard.swift
@@ -0,0 +1,29 @@
+//
+//  WallpaperCard.swift
+//  Wallify
+//
+//  Created by BERKAY BARLAS on 27.11.2023.
+//
+
+import SwiftUI
+
+struct WallpaperCard: View {
+    let image: Image
+    let size: CGFloat
+    var body: some View {
+        VStack{
+            image
+                .resizable()
+                .frame(width: size, height: 200 * (size/210))
+                .cornerRadius(20.0)
+            
+            Text("Three Lux")
+                .font(.title3)
+                .fontWeight(.medium)
+        }
+        .frame(width: size)
+        .padding()
+        .background(Colors.cardBG)
+        .cornerRadius(20.0)
+    }
+}
