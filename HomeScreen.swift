d5ccd53ba705335cbc1bfb985f57027dce9fafe6
Berkay Barlas <74761090+berkaybarlass@users.noreply.github.com>
2023-11-29 21:38:12 +0300

    [Screen] | HomeScreen.swift "Changed color name and file formatted"

--- HomeScreen.swift
+++ HomeScreen.swift
@@ -11,61 +11,61 @@ struct HomeScreen: View {
     var body: some View {
         NavigationView {
             ZStack {
-                Colors.firstBg
+                Colors.primaryBgColor
                     .ignoresSafeArea(.all)
-                VStack{
+                VStack {
                     HeaderText()
-                    
+
                     VStack (alignment: .leading) {
-                        VStack{
+                        VStack {
                             Text("Popular")
-                            
+
                                 .padding(.horizontal)
                                 .font(.title2)
                                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            
+
                         }
-                        .padding()
+                            .padding()
                         ScrollView(.horizontal, showsIndicators: false) {
                             HStack (spacing: 0) {
                                 ForEach(0..<4) { i in
                                     NavigationLink(destination: DetailScreen(), label: {
-                                        WallpaperCard(image: Image("homePageWallpaper_\(i + 1)"), size: 210)
-                                    })
-                                    .navigationBarHidden(true)
-                                    .foregroundColor(.black)
-                                    
+                                            WallpaperCard(image: Image("homePageWallpaper_\(i + 1)"), size: 210)
+                                        })
+                                        .navigationBarHidden(true)
+                                        .foregroundColor(.black)
+
                                 }
-                                .padding(.leading)
+                                    .padding(.leading)
                             }
                         }
-                        .padding(.bottom)
-                        
-                        VStack (alignment: .leading){
+                            .padding(.bottom)
+
+                        VStack (alignment: .leading) {
                             Text("Best")
                                 .padding(.horizontal)
                                 .font(.title2)
                                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                         }
-                        
+
                         ScrollView(.horizontal, showsIndicators: false) {
                             HStack (spacing: 0) {
                                 ForEach(0..<4) { i in
                                     NavigationLink(destination: DetailScreen(), label: {
-                                        WallpaperCard(image: Image("bestWP_\(i + 1)"), size: 210)
-                                    })
-                                    .navigationBarHidden(true)
-                                    .foregroundColor(.black)
-                                    
+                                            WallpaperCard(image: Image("bestWP_\(i + 1)"), size: 210)
+                                        })
+                                        .navigationBarHidden(true)
+                                        .foregroundColor(.black)
+
                                 }
-                                .padding(.leading)
+                                    .padding(.leading)
                             }
                         }
-                        .padding(.bottom)
-                        
-                        
+                            .padding(.bottom)
+
+
                     }
-                    
+
                 }
 
             }
