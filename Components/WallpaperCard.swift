a6a2da7fbc7e0b43c841d25e0496dd287ad80f13
Berkay Barlas <74761090+berkaybarlass@users.noreply.github.com>
2023-11-29 21:37:43 +0300

    [Components] | Wallpaper.swift "changed color name and file formatted"

--- Components/WallpaperCard.swift
+++ Components/WallpaperCard.swift
@@ -11,19 +11,19 @@ struct WallpaperCard: View {
     let image: Image
     let size: CGFloat
     var body: some View {
-        VStack{
+        VStack {
             image
                 .resizable()
-                .frame(width: size, height: 200 * (size/210))
+                .frame(width: size, height: 200 * (size / 210))
                 .cornerRadius(20.0)
-            
+
             Text("Three Lux")
                 .font(.title3)
                 .fontWeight(.medium)
         }
-        .frame(width: size)
-        .padding()
-        .background(Colors.cardBG)
-        .cornerRadius(20.0)
+            .frame(width: size)
+            .padding()
+            .background(Colors.smallWpCardBg)
+            .cornerRadius(20.0)
     }
 }
